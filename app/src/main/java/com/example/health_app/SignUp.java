a02f5e16e924152b5d02623c7e5d0d5079b75ca7
sahilsgovekar <sahilsgovekar@gmail.com>
2023-06-23 00:04:34 +0530

    added password validation

--- app/src/main/java/com/example/health_app/SignUp.java
+++ app/src/main/java/com/example/health_app/SignUp.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 
 import com.example.health_app.database.MyDbHelper;
 
+import java.util.regex.Pattern;
+
 public class SignUp extends AppCompatActivity {
 
     EditText username, password, repassword;
@@ -44,20 +46,22 @@ public class SignUp extends AppCompatActivity {
                 }
                 else {
                     if( pass.equals(repass)) {
-                        Boolean checkuser = db.checkusername(user);
-                        if(checkuser == false) {
-                            Boolean insert = db.insertData(user, pass);
-                            if(insert==true) {
-                                Toast.makeText(SignUp.this, "regrestration sucessfull", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), SignupPerMedDet.class);
-                                intent.putExtra("username", user);
-                                startActivity(intent);
-                                finish();
+                        if(isValidPassword(pass)) {
+                            Boolean checkuser = db.checkusername(user);
+                            if(checkuser == false) {
+                                Boolean insert = db.insertData(user, pass);
+                                if(insert==true) {
+                                    Toast.makeText(SignUp.this, "regrestration sucessfull", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), SignupPerMedDet.class);
+                                    intent.putExtra("username", user);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(SignUp.this, "Regrestration failes, try again", Toast.LENGTH_SHORT).show();
+                                }
                             } else {
-                                Toast.makeText(SignUp.this, "Regrestration failes, try again", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignUp.this, "user already exist", Toast.LENGTH_SHORT).show();
                             }
-                        } else {
-                            Toast.makeText(SignUp.this, "user already exist", Toast.LENGTH_SHORT).show();
                         }
                     } else {
                         Toast.makeText(SignUp.this, "password not matching", Toast.LENGTH_SHORT).show();
@@ -76,5 +80,34 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    private Boolean isValidPassword(String password) {
+        Pattern lowercase = Pattern.compile("^.*[a-z].*$");
+        Pattern uppercase = Pattern.compile("^.*[A-Z].*$");
+        Pattern number = Pattern.compile("^.*[0-9].*$");
+        Pattern specialCharacter = Pattern.compile("^.*[^a-zA-Z0-9].*$");
+        if (password.length() < 8) {
+            Toast.makeText(this, "password should be minimum of 8 character", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!lowercase.matcher(password).matches()) {
+            Toast.makeText(this, "password should contains one lower case", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!uppercase.matcher(password).matches()) {
+            Toast.makeText(this, "password should contains atleast one upper case", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!number.matcher(password).matches()) {
+            Toast.makeText(this, "Toast should contain atleast one number", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!specialCharacter.matcher(password).matches()) {
+            Toast.makeText(this, "Password should contain atleast one special character", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
